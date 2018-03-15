e72b035c781a1afa445b9a99a1edc5863d5334d8
zobrist <rodublin@up.edu.ph>
2018-03-15 19:19:52 +0800

    Fixed Post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315053808) do
+ActiveRecord::Schema.define(version: 20180315111803) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "user"
